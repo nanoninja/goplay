fa0e2c2641b3a6235e03cc49012d1c363fea2260
Vincent Letourneau <vincent@nanoninja.com>
2014-06-09 16:43:53 +0200

    Adding net package

---
+++ net/http/file_server.go
@@ -0,0 +1,30 @@
+// Copyright 2014 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net/http"
+)
+
+var hostname string
+var port int
+var root string
+
+func init() {
+	flag.StringVar(&root, "root", "/", "Define the root path. Example: /your/root/path/")
+	flag.StringVar(&hostname, "hostname", "localhost", "Define the server name. Example: localhost")
+	flag.IntVar(&port, "port", 8080, "Define the port of the server. Example: 80")
+	flag.Parse()
+}
+
+func main() {
+	serverName := fmt.Sprintf("%s:%d", hostname, port)
+	err := http.ListenAndServe(serverName, http.FileServer(http.Dir(root)))
+	if err != nil {
+		panic(err)
+	}
+}
