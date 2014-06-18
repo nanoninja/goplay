746b1f543df90a9d7d4bed706b865a128a4d2939
Vincent Letourneau <vincent@nanoninja.com>
2014-06-18 21:22:47 +0200

    Adding compile

---
+++ compile/cross/windows/amd64.bash
@@ -0,0 +1,13 @@
+# How to compile your application for windows amd64
+# Set up cross compilation to windows amd64
+
+cd $GOROOT/src
+GOOS=windows GOARCH=amd64 ./make.bash --no-clean
+
+cd $YOUR_APP_PATH
+
+# Simple compilation
+GOOS=windows GOARCH=amd64 go build main.go
+
+# Compilation with a different output
+GOOS=windows GOARCH=amd64 go build -o yourAppName.exe main.go
