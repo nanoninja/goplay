4561a28da2c2a21c4ca6d35cc86686e9006d676c
Nanoninja <vincent@nanoninja.com>
2014-07-20 16:18:25 +0200

    Add another method to build on mac

--- compile/cross/windows/amd64.bash
+++ compile/cross/windows/amd64.bash
@@ -11,3 +11,11 @@ GOOS=windows GOARCH=amd64 go build main.go
 
 # Compilation with a different output
 GOOS=windows GOARCH=amd64 go build -o yourAppName.exe main.go
+
+
+# Another possibility on mac
+sudo env GOOS=windows go tool dist bootstrap -v
+go build -o yourAppName.exe main.go
+
+# Check bin
+file yourAppName.exe
